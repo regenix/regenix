4af84322851f68b6a5bdc6a350bf3efa93556f9f
Dmitriy Zayceff <dmitry.zaycev@esector.ru>
2013-04-19 10:12:56 +0500

    Add i18n js file option disable/enable

--- framework/Project.php
+++ framework/Project.php
@@ -213,11 +213,14 @@ class Project {
     }
 
     private function _registerSystemController(){
-        if ($this->config->getBoolean('captcha.enable'))
+        if ($this->config->getBoolean('captcha.enable')){
             $this->router->addRoute('GET', Captcha::URL, 'framework.mvc.SystemController.captcha');
+        }
 
-        $this->router->addRoute('GET', '/system/i18n.js', 'framework.mvc.SystemController.i18n_js');
-        $this->router->addRoute('GET', '/system/i18n.{_lang}.js', 'framework.mvc.SystemController.i18n_js');
+        if ($this->config->getBoolean('i18n.js')){
+            $this->router->addRoute('GET', '/system/i18n.js', 'framework.mvc.SystemController.i18n_js');
+            $this->router->addRoute('GET', '/system/i18n.{_lang}.js', 'framework.mvc.SystemController.i18n_js');
+        }
     }
 
     private function _registerTests(){
