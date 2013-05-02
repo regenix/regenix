7c27f78d423c38f28c8b5f3736f4bec0507f351c
Dmitriy Zayceff <dmitry.zaycev@esector.ru>
2013-05-02 18:21:25 +0500

    Improve test project

--- apps/project1/app/controllers/Application.php
+++ apps/project1/app/controllers/Application.php
@@ -2,12 +2,6 @@
 namespace controllers;
 
 use framework\Project;
-use framework\deps\GithubOrigin;
-use framework\deps\Repository;
-use framework\lang\FrameworkClassLoader;
-use framework\libs\Captcha;
-use framework\libs\I18n;
-use framework\libs\WS;
 use framework\logger\Logger;
 use framework\mvc\Controller;
 use framework\mvc\RequestQuery;
@@ -16,10 +10,6 @@ use framework\libs\ImageUtils;
 class Application extends Controller {
 
     public function index(){
-        $repository = new Repository(Project::current()->deps);
-        $repository->setEnv('assets');
-        $repository->download('jquery', '2.*');
-
-        $this->renderJSON('OK');
+        $this->render();
     }
 }
