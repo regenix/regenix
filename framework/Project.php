10013377a41666e073ee6adc2eb583b6f0d4cb26
dim-s <dz@dim-s.net>
2013-05-12 15:04:00 +0500

    Remove print_r debug info

--- framework/Project.php
+++ framework/Project.php
@@ -150,7 +150,6 @@ class Project {
      */
     public function findCurrentPath(){
         $request = Request::current();
-        dump($request);
         foreach ($this->paths as $url){
             if ( $request->isBase( $url ) )
                 return $url;
