727e5c16cd2f66ececc3a7d467b728874f4c6027
dim-s <dz@dim-s.net>
2013-05-12 13:37:41 +0500

    Add regenix site project

---
+++ apps/regenix-site/app/controllers/AppController.php
@@ -0,0 +1,8 @@
+<?php
+namespace controllers;
+
+use framework\mvc\Controller;
+
+abstract class AppController extends Controller {
+
+}
---
+++ apps/regenix-site/app/controllers/Application.php
@@ -0,0 +1,10 @@
+<?php
+namespace controllers;
+
+
+class Application extends AppController {
+
+    public function index(){
+        $this->render();
+    }
+}
