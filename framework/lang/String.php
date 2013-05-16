f2923e1b1432c52e2d3724fab9e9026b8d1b5e46
Dmitriy Zayceff <dmitry.zaycev@esector.ru>
2013-05-16 15:31:03 +0500

    Improve string class for random generation

--- framework/lang/String.php
+++ framework/lang/String.php
@@ -49,4 +49,41 @@ abstract class String {
         // TODO optimize ?
         return substr($string, -strlen($with)) === $with;
     }
+
+    private static $alpha   = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    private static $numeric = '0123456789';
+    private static $symbol  = '~!@#$%^&*+/-*_=';
+
+    /**
+     * @param int $length
+     * @param bool $withNumeric
+     * @param bool $withSpecSymbol
+     * @return string
+     */
+    public static function random($length, $withNumeric = true, $withSpecSymbol = false){
+        $characters = self::$alpha;
+        if ($withNumeric)
+            $characters .= self::$numeric;
+        if ($withSpecSymbol)
+            $characters .= self::$symbol;
+
+        $randomString = '';
+        $len = strlen($characters);
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[mt_rand(0, $len - 1)];
+        }
+        return $randomString;
+    }
+
+    /**
+     * @param $lengthFrom
+     * @param $lengthTo
+     * @param bool $withNumeric
+     * @param bool $withSpecSymbol
+     * @return string
+     */
+    public static function randomRandom($lengthFrom, $lengthTo, $withNumeric = true, $withSpecSymbol = false){
+        $length = mt_rand($lengthFrom, $lengthTo);
+        return self::random($length, $withNumeric, $withSpecSymbol);
+    }
 }
