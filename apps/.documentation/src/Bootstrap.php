51051ec5fe32901e3b181002df6c254eeb86d62a
Dmitriy Zayceff <dz@dim-s.net>
2013-10-18 14:04:39 +0500

    [doc app] Improve switching app mode

--- apps/.documentation/src/Bootstrap.php
+++ apps/.documentation/src/Bootstrap.php
@@ -2,9 +2,16 @@
 namespace {
 
     use regenix\core\AbstractBootstrap;
+    use regenix\mvc\http\Request;
     use regenix\mvc\template\TemplateLoader;
 
     class Bootstrap extends AbstractBootstrap {
+
+        public function onEnvironment(&$env) {
+            $request = Request::getInstance();
+            if ($request->isBase('http://regenix.ru'))
+                $env = 'prod';
+        }
 
         public function onUseTemplates(){
             TemplateLoader::registerPath(ROOT . 'documentation/');
